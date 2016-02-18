56acd35652dc3496e579e61b64e8198fb653a24a
martin-varbanov96 <martin_varbanov96@abv.bg>
2016-02-19 00:00:10 +0200

    data- example

---
+++ html data tag/project/script/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var width = $("#rectangle").attr("data-width");
+    var height = $("#rectangle").attr("data-height");
+    $("body").append("<div>The width of the rectangle is " + width + " and the height is " + height + " </div>");
+
+    
+});
